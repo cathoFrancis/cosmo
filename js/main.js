d31eb01e7e89ebd2790bc99160110a281d85251f
Igor <catho.francis@yandex.ru>
2021-08-22 20:41:34 +0300

    project cosmo

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function () {
+   $('.slider__box').slick({
+      prevArrow: '<img class="slider__arrow slider__arrow-left" src="images/arrow-l.svg" alt="left">',
+      nextArrow: '<img class="slider__arrow slider__arrow-right" src="images/arrow-r.svg" alt="right">',
+      responsive: [
+         {
+            breakpoint: 481,
+            settings: {
+               arrows: false
+            }
+         }
+      ]
+   });
+
+   $('.menu-btn').on('click', function () {
+      $('.menu__list').toggleClass('active');
+   });
+
+});
